4c3fe43346cb2184740545111c9292d0f1056033
Martha Liliana Dominguez <marthadominguezg@gmail.com>
2022-07-26 10:31:34 -0500

    basic css layout started

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
-const API_URL = "https://api.thedogapi.com/v1/images/search";
-console.log("hola!!!")
 
-async function reload() {
-    const res = await fetch(API_URL);
-    const data = await res.json();
-    const img = document.querySelector("img");
+const API_URL_RANDOM = "https://api.thedogapi.com/v1/images/search?";
+const API_URL_FAV = "https://api.thedogapi.com/v1/favourites&api_key=e17848ad-e0cc-4142-aa5c-abbbfae0f372";
+
+// fetch nos devuelve una promesa, y las promesas las podemos resolver con el método THEN. 
+/*
+fetch(API_URL).then(res=> res.json())
+.then(data => {
+    const img = document.querySelector('img');
     img.src = data[0].url;
+})
+*/
+
+const spanError = document.getElementById("error");
+
+async function loadRandomDogs() {
+    const res = await fetch(API_URL_RANDOM);
+    const data = await res.json();
+
+    if (res.status !== 200) {
+        spanError.innerHTML = "Ups! Error " + res.status;
+    }
+    else {
+        // si el status code sí es exitoso
+        const img = document.querySelector("img");
+        img.src = data[0].url;
+    }
 }
 
-reload();
+loadRandomDogs()
 
-/* NOTAS
+async function loadFavDogs() {
+    const res = await fetch(API_URL_FAV);
+    const data = await res.json();
+    console.log(data);
+}
 
-- implementar endpoints y query parameters, este depende de la API. 
+loadFavDogs()
 
-HTTP Status Codes
-    1XX Respuestas Afirmativas
-    2XX Respuestas satisfactorias
-    3XX Re-direcciones
-    4XX Error del cliente
-    5XX Error de servidor
-
-- Qué es una API KEY: Son un método de autenticación para identificar quién está haciendo la solicitud a los endpoints de una API. 
-
-Cómo se envían las API Keys? Se pueden enviar de muchas maneras, a través del authorization header, a través de un query parameter (en la API_URL), a través de Basic Auth, Body Data, Custom Header, etc. 
-
-
-
-*/
+// tenemos que enviar un message en el body, por defecto si no le envíamos método va a ser un GET
+async function saveFavDogs() {
+    const res = await fetch(API_URL_FAV);
+    const data = await res.json();
+    
+}
